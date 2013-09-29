b1553965e99ca8bfc7a80443045a2871b26755b2
Justin Dray <justin@dray.be>
2013-09-29 22:58:50 +1000

    fixed syntax

--- modules/os_default/manifests/ntp.pp
+++ modules/os_default/manifests/ntp.pp
@@ -1,10 +1,12 @@
 class ntp {
   package { 'openntpd': ensure => installed }
+
   file { '/etc/localtime':
     ensure  => link,
     require => Package['openntpd'],
     target  => '/usr/share/zoneinfo/Australia/Brisbane',
   }
+
   service { 'openntpd':
     ensure    => running,
     enable    => true,
