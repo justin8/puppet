00687604e7fe12731ac25ba4a10f2b43b2e0ac03
Justin Dray <justin@dray.be>
2013-10-01 08:59:17 +1000

    Moved settings storage to be local with only Thumbnails shared over SMB. Puppet manages keeping config in sync now

--- modules/xbmc/manifests/init.pp
+++ modules/xbmc/manifests/init.pp
@@ -2,66 +2,6 @@ class xbmc( $user = 'xbmc', $standalone = 'true') {
   $packages = [ 'ethtool', 'git', 'polkit', 'udisks', 'xbmc-git' ]
   package { $packages: ensure => installed }
 
-  file { '/mnt/xbmc':
-    ensure => directory,
-    owner  => $user,
-    group  => $user,
-  }
-
-  mount { '/mnt/xbmc':
-    device  => '//abachi/XBMC',
-    fstype  => 'cifs',
-    options => "credentials=/root/.smbcreds,noauto,x-systemd.automount,uid=$user,gid=$user",
-    ensure  => mounted,
-    atboot  => true,
-    require => File['/mnt/xbmc'],
-  }
-
-  file { "/mnt/xbmc/$fqdn":
-    ensure  => directory,
-    recurse => true,
-    force   => true,
-    source  => 'puppet:///modules/xbmc/template',
-  }
-
-  if $user == 'xbmc' {
-    file { '/var/lib/xbmc/.xbmc':
-      ensure => directory,
-      owner  => $user,
-      group  => $user,
-    }
-
-    mount { '/var/lib/xbmc/.xbmc':
-      device  => "/mnt/xbmc/$fqdn",
-      fstype  => 'none',
-      options => 'bind,noauto,x-systemd.automount',
-      ensure  => mounted,
-      atboot  => true,
-      require => Mount['/mnt/xbmc'],
-    }
-  }
-  else {
-    file { "/home/$user/.xbmc":
-      ensure => directory,
-      owner  => $user,
-      group  => $user,
-    }
-
-    mount { "/home/$user/.xbmc":
-      device  => "/mnt/xbmc/$fqdn",
-      fstype  => 'none',
-      options => 'bind,noauto,x-systemd.automount',
-      ensure  => mounted,
-      atboot  => true,
-      require => Mount['/mnt/xbmc'],
-    }
-  }
-
-  file { "/mnt/xbmc/$fqdn/userdata/Thumbnails":
-    ensure  => '/mnt/xbmc/template/userdata/Thumbnails',
-    require => File["/mnt/xbmc/$fqdn"],
-  }
-
   file { '/usr/share/xbmc/addons/skin.confluence/720p/IncludesHomeMenuItems.xml':
     ensure  => file,
     owner   => root,
@@ -76,6 +16,59 @@ class xbmc( $user = 'xbmc', $standalone = 'true') {
     group   => root,
     mode    => '0664',
     source  => 'puppet:///modules/xbmc/polkit_10-xbmc.rules',
+  }
+
+  if $user == 'xbmc' {
+    file { '/var/lib/xbmc/.xbmc':
+      ensure  => directory,
+      recurse => true,
+      force   => true,
+      owner   => $user,
+      group   => $user,
+      source  => 'puppet:///modules/xbmc/shared-settings',
+    }
+
+    file { '/var/lib/xbmc/.xbmc/userdata/Thumbnails':
+      ensure  => directory,
+      owner   => $user,
+      group   => $user,
+      require => File['/var/lib/xbmc/.xbmc'],
+    }
+
+    mount { '/var/lib/xbmc/.xbmc/userdata/Thumbnails':
+      device  => '//abachi/XBMC-Thumbnails',
+      fstype  => 'cifs',
+      options => 'credentials=/root/.smbcreds,noauto,x-systemd.automount,uid=$user,gid=$user',
+      ensure  => mounted,
+      atboot  => true,
+      require => File['/var/lib/xbmc/.xbmc/userdata/Thumbnails"],
+    }
+  }
+  else {
+    file { "/home/$user/.xbmc":
+      ensure   => directory,
+      recurse  => true,
+      force    => true,
+      owner    => $user,
+      group    => $user,
+      source   => "puppet:///modules/xbmc/shared-settings",
+    }
+
+    file { "/home/$user/.xbmc/userdata/Thumbnails":
+      ensure  => directory,
+      owner   => $user,
+      group   => $user,
+      require => File["/home/$user/.xbmc"],
+    }
+
+    mount { "/home/$user/.xbmc/userdata/Thumbnails":
+      device  => '//abachi/XBMC-Thumbnails',
+      fstype  => 'cifs',
+      options => 'credentials=/root/.smbcreds,noauto,x-systemd.automount,uid=$user,gid=$user',
+      ensure  => mounted,
+      atboot  => true,
+      require => File["/home/$user/.xbmc/userdata/Thumbnails"],
+    }
   }
 
   # Config purely for standalone setups
