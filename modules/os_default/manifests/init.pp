209c3bea2061792530fc0609a02966dabbe87ac4
Justin Dray <justin@dray.be>
2013-09-30 14:36:00 +1000

    Moved WOL to os_defaults

--- modules/os_default/manifests/init.pp
+++ modules/os_default/manifests/init.pp
@@ -3,4 +3,12 @@ class os_default {
   include ntp
   include optimizations
   include sudo
+
+  file { '/etc/udev/rules.d/50-wol.rules':
+    ensure => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0664',
+    source  => 'puppet:///modules/os_default/udev_50-wol.rules',
+  }
 }
