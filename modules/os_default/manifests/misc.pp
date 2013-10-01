874eb034a8b58c47aca7aed99b25b4d8be3179a1
Justin Dray <justin@dray.be>
2013-10-02 09:32:24 +1000

    Speedups for misc/etc sections

--- modules/os_default/manifests/misc.pp
+++ modules/os_default/manifests/misc.pp
@@ -1,31 +1,29 @@
 class misc {
   file { '/usr/local/bin/colourdiff':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
     mode    => '0775',
     source  => 'puppet:///modules/os_default/colourdiff',
   }
 
   file { '/etc/puppet/puppet.conf':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
     mode    => '0664',
     source  => 'puppet:///modules/os_default/puppet.conf',
   }
 
   file { '/etc/udev/rules.d/50-wol.rules':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
     mode    => '0664',
     source  => 'puppet:///modules/os_default/udev_50-wol.rules',
   }
 
-  file { '/etc':
-    ensure  => directory,
-    recurse => true,
-    source  => 'puppet:///modules/os_default/etc',
+  file { '/etc/bash.bashrc':
+    ensure  => file,
+    source  => 'puppet:///modules/os_default/etc/bash.bashrc',
+  }
+
+  file { '/etc/DIR_COLORS':
+    ensure  => file,
+    source  => 'puppet:///modules/os_default/etc/DIR_COLORS',
   }
 }
