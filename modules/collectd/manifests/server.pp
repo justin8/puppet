c26ff5420ecd77548fbca88c548b6c6dc4467007
Justin Dray <justin@dray.be>
2013-10-07 16:40:20 +1000

    Fixed typo

--- modules/collectd/manifests/server.pp
+++ modules/collectd/manifests/server.pp
@@ -2,7 +2,7 @@ class collectd::server inherits collectd {
    @file { '/etc/collectd.d/network.conf':
     ensure  => file,
     mode    => '0644',
-    source  => 'puppet:///modules/collectd/collectd.d/network.conf-server'),
+    source  => 'puppet:///modules/collectd/collectd.d/network.conf-server',
     require => [ Package['collectd'], File['/etc/collectd.d'] ],
     notify  => Service['collectd'],
   }
