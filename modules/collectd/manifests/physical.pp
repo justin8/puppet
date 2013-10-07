efa4bc6784ad557c16c50da79704658d921d9245
Justin Dray <justin@dray.be>
2013-10-07 20:22:36 +1000

    Fixed typo in physical manifest

--- modules/collectd/manifests/physical.pp
+++ modules/collectd/manifests/physical.pp
@@ -2,7 +2,7 @@ class collectd::physical {
 
   file { '/etc/collectd.d/physical.conf':
     ensure  => file,
-    source  => 'puppet:///modules/collectd/collectd.d/phsyical.conf',
+    source  => 'puppet:///modules/collectd/collectd.d/physical.conf',
     mode    => '0644',
     require => [ Package['collectd'], File['/etc/collectd.d'] ],
     notify  => Service['collectd'],
