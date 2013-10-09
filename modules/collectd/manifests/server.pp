64f942a0ec235f421b68a1929acfd3d6c72ef95d
Justin Dray <justin@dray.be>
2013-10-09 13:08:47 +1000

    Added rrdtool as required package

--- modules/collectd/manifests/server.pp
+++ modules/collectd/manifests/server.pp
@@ -1,9 +1,13 @@
 class collectd::server inherits collectd {
-   File['/etc/collectd.d/network.conf'] {
+  package { 'rrdtool':
+    ensure => installed,
+  }
+
+  File['/etc/collectd.d/network.conf'] {
     ensure  => file,
     mode    => '0644',
     source  => 'puppet:///modules/collectd/collectd.d/network.conf-server',
-    require => [ Package['collectd'], File['/etc/collectd.d'] ],
+    require => [ Package['collectd'], Package['rrdtool'] File['/etc/collectd.d'] ],
     notify  => Service['collectd'],
   }
 
