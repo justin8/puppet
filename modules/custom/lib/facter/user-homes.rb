ea4877e572aaf6940bcea93e12fe78e35318ece3
Justin Dray <justin@dray.be>
2013-10-10 11:11:50 +1000

    Moved home check to a ruby fact

---
+++ modules/custom/lib/facter/user-homes.rb
@@ -0,0 +1,10 @@
+require 'puppet'
+
+Facter::Util::Resolution.exec("getent passwd|awk -F: '{ print $1\"=\"$6}'").split("\n").each do |x|
+        user, home = x.split('=')
+        Facter.add("home_" + user) do
+                setcode do
+                        home
+                end
+        end
+end
