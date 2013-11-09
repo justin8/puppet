9c185e1a17f4e8a953acdb02361f2802a85f9148
Justin Dray <justin@dray.be>
2013-11-09 14:46:22 +1000

    os_default:Removed old puppet fact cleanup section

--- modules/os_default/manifests/init.pp
+++ modules/os_default/manifests/init.pp
@@ -5,12 +5,6 @@ class os_default {
   include os_default::optimizations
   include os_default::sudo
 
-  # Temporary cleanup of old facts
-  file { '/etc/facter':
-    ensure => absent,
-    force  => true,
-  }
-
   #$packages = [ 'foo', 'bar' ]
   #package { $packages: ensure => installed }
 }
