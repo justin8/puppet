a3686666331b2baf2af871604f5109bc7dd9d4f9
Justin Dray <justin@dray.be>
2013-11-09 15:04:06 +1000

    fixed typos

--- modules/blog/manifests/init.pp
+++ modules/blog/manifests/init.pp
@@ -1,16 +1,17 @@
 class blog {
   include blog::packages
 
-  file '/etc/php/php.ini':
+  file { '/etc/php/php.ini':
     ensure  => present,
     require => Package['php'],
     source  => 'puppet:///modules/blog/etc/php/php.ini',
   }
 
-  file '/usr/share/webapps/phpMyAdmin/config':
+  file { '/usr/share/webapps/phpMyAdmin/config':
     ensure  => directory,
     mode    => '0775',
     owner   => 'http',
     group   => 'http',
     require => [ Package['phpmyadmin'], Package['apache'],
+  }
 }
