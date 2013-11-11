a9619fe69250455b9fbe3839a9d854aef6501b2c
Justin Dray <justin@dray.be>
2013-11-11 10:32:41 +1000

    os_default: Added locale config. Cleaned up misc manifest

--- modules/os_default/manifests/misc.pp
+++ modules/os_default/manifests/misc.pp
@@ -1,29 +1,39 @@
 class os_default::misc {
-  file { '/usr/local/bin/colourdiff':
-    ensure  => file,
-    mode    => '0775',
-    source  => 'puppet:///modules/os_default/colourdiff',
+  file {
+    '/usr/local/bin/colourdiff':
+      ensure  => file,
+      mode    => '0775',
+      source  => 'puppet:///modules/os_default/colourdiff';
+    '/etc/puppet/puppet.conf':
+      ensure  => file,
+      mode    => '0664',
+      source  => 'puppet:///modules/os_default/puppet.conf';
+
+    '/etc/udev/rules.d/50-wol.rules':
+      ensure  => file,
+      mode    => '0664',
+      source  => 'puppet:///modules/os_default/udev_50-wol.rules';
+
+    '/etc/bash.bashrc':
+      ensure  => file,
+      source  => 'puppet:///modules/os_default/etc/bash.bashrc';
+
+    '/etc/DIR_COLORS':
+      ensure  => file,
+      source  => 'puppet:///modules/os_default/etc/DIR_COLORS';
+
+    '/etc/locale.conf':
+      ensure  => file,
+      source  => 'puppet:///modules/os_default/etc/locale.conf';
+
+    '/etc/locale.gen':
+      ensure  => file,
+      source  => 'puppet:///modules/os_default/etc/locale.gen';
   }
 
-  file { '/etc/puppet/puppet.conf':
-    ensure  => file,
-    mode    => '0664',
-    source  => 'puppet:///modules/os_default/puppet.conf',
-  }
-
-  file { '/etc/udev/rules.d/50-wol.rules':
-    ensure  => file,
-    mode    => '0664',
-    source  => 'puppet:///modules/os_default/udev_50-wol.rules',
-  }
-
-  file { '/etc/bash.bashrc':
-    ensure  => file,
-    source  => 'puppet:///modules/os_default/etc/bash.bashrc',
-  }
-
-  file { '/etc/DIR_COLORS':
-    ensure  => file,
-    source  => 'puppet:///modules/os_default/etc/DIR_COLORS',
+  exec { 'locale-gen':
+    command     => '/usr/bin/locale-gen',
+    subscribe   => File['/etc/locale.gen'],
+    refreshonly => true,
   }
 }
