65e3b37bcb4839ad4c3e8eab8ed763071432a8ea
Justin Dray <justin@dray.be>
2013-12-09 20:07:36 +1000

    Removed missing reference to build-new-packages script

--- modules/repo/manifests/init.pp
+++ modules/repo/manifests/init.pp
@@ -10,22 +10,19 @@ class repo {
     '/srv/repo':
       ensure => directory;
 
+    '/etc/incron.allow':
+      ensure  => file,
+      content => "root\n";
+
     '/etc/incron.d/update-repo':
       ensure  => file,
       source  => 'puppet:///modules/repo/update-repo.incron',
       require => File['/usr/local/bin/update-repo'];
 
-    '/etc/incron.allow':
-      ensure  => file,
-      content => "root\n";
     '/usr/local/bin/update-repo':
       ensure => file,
       mode   => '0755',
       source => 'puppet:///modules/repo/update-repo';
-    '/usr/local/bin/build-new-packages':
-      ensure => file,
-      mode   => '0755',
-      source => 'puppet:///modules/repo/build-new-packages';
   }
 
 }
