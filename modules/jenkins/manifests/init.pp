128fa5b31d1d4e3af2478ab21438ffe6f2e56a4c
Justin Dray <justin@dray.be>
2013-12-09 22:28:45 +1000

    Fixed realization of jenkins vhost

--- modules/jenkins/manifests/init.pp
+++ modules/jenkins/manifests/init.pp
@@ -3,11 +3,6 @@ class jenkins {
   $packages = [ 'jenkins-ci' ]
   package { $packages: ensure => installed }
 
-  include httpd
-  realize(
-    Httpd::Vhost['jenkins.dray.be'],
-  )
-
   service {
     'jenkins':
       ensure => running,
