07a2443c5c40176b0c7d4a3d6e7b7544e6169bba
Justin Dray <justin@dray.be>
2013-12-09 20:26:50 +1000

    jenkins: Added service

--- modules/jenkins/manifests/init.pp
+++ modules/jenkins/manifests/init.pp
@@ -3,9 +3,16 @@ class jenkins {
   $packages = [ 'jenkins-ci' ]
   package { $packages: ensure => installed }
 
+  service {
+    'jenkins':
+      ensure => running,
+      enable => true;
+  }
+
   file {
     '/etc/conf.d/jenkins':
       ensure => file,
-      source => 'puppet:///modules/jenkins/conf.d-jenkins';
+      source => 'puppet:///modules/jenkins/conf.d-jenkins'
+      notify => Service['jenkins'];
   }
 }
