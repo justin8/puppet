b67156b006cf3a7b413fee9f9e4e45a59d7f1d28
Justin Dray <justin@dray.be>
2013-12-14 15:04:22 +1000

    Added dray.be repo config to os_default::pacman

--- modules/os_default/manifests/pacman.pp
+++ modules/os_default/manifests/pacman.pp
@@ -3,6 +3,13 @@ class os_default::pacman {
   $packages = [ 'cifs-utils', 'smbclient' ]
   package { $packages: ensure => installed }
 
+  exec { 'configure-repo':
+    path    => '/usr/bin',
+    unless  => 'pacman -Q dray-repo > /dev/null 2>&1',
+    command => 'curl -s "https://repo.dray.be/any/dray-repo-0.5-1-any.pkg.tar.xz" > /tmp/dray-repo.pkg.tar.xz \
+                && pacman -U /tmp/dray-repo.pkg.tar.xz';
+  }
+
   if "$local" == "true" {
     if "$hostname" != "abachi" {
       mount { "/var/cache/pacman/pkg":
