1d1ed458f537f6c6569b3ecd0012fb3e8f474bbb
Justin Dray <justin@dray.be>
2013-12-29 20:47:17 +1000

    Changed to use the packaged openntpd networkmanager-dispatcher script

--- modules/os_default/manifests/ntp.pp
+++ modules/os_default/manifests/ntp.pp
@@ -10,9 +10,13 @@ class os_default::ntp {
   }
 
   if $networkmanager == 'true' {
+#This file directive can be removed in the future
     file { '/etc/NetworkManager/dispatcher.d/restart-openntpd':
-      ensure  => present,
-      source  => 'puppet:///modules/os_default/nmd-restart-openntpd';
+      ensure  => absent;
+    }
+
+    package { 'networkmanager-dispatcher-openntpd':
+      ensure => installed;
     }
 
     service { 'NetworkManager-dispatcher':
