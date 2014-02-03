fdbe25b35d6c7579e30f377ff99a6596a42c60d1
Justin Dray <justin@dray.be>
2014-02-03 17:37:55 +1000

    Fixed local fact to work as intended

--- modules/custom/lib/facter/local.rb
+++ modules/custom/lib/facter/local.rb
@@ -1,7 +1,7 @@
 require 'puppet'
 
 local="false"
-if Facter::Util::Resolution.exec("nslookup abachi.dray.be|grep -q 192.168.1.15")
+if Facter::Util::Resolution.exec("nslookup abachi.dray.be|grep 192.168.1.15")
 	local="true"
 end
 Facter.add("local") do
