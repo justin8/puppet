2394fd032f9ae7b00305bbfeaddaefccdfb876b7
Justin Dray <justin@dray.be>
2014-03-16 10:35:39 +1000

    Customized cgminer service file

--- modules/doge/manifests/init.pp
+++ modules/doge/manifests/init.pp
@@ -1,23 +1,35 @@
 class doge {
 
-  $packages = [ 'amdapp-sdk', 'catalyst-hook', 'cgminer-gpu', 'ncurses' ]
-  package { $packages: ensure => installed }
+    $packages = [ 'amdapp-sdk', 'catalyst-hook', 'cgminer-gpu', 'ncurses' ]
+        package { $packages: ensure => installed }
 
-  file {
-    '/etc/cgminer.conf':
-        mode    => '0664',
-        content => template('doge/cgminer.conf.erb'),
-        notify  => Service['cgminer']
-  }
+    file {
+        '/etc/cgminer.conf':
+            mode    => '0664',
+            content => template('doge/cgminer.conf.erb'),
+            notify  => Service['cgminer'];
 
-  service {
-    'cgminer':
-      ensure => running,
-      enable => true;
+        '/etc/systemd/system/cgminer.service':
+            mode   => '0664',
+            source => 'puppet:///modules/doge/cgminer.service',
+            notify => [ Exec['reload-daemon-config'], Service['cgminer'] ],
+    }
 
-    'catalyst-hook':
-      ensure => running,
-      enable => true;
-  }
+    exec { 'reload-daemon-config':
+        refreshonly => true,
+        path        => '/usr/bin',
+        command     => 'systemctl --system daemon-reload'
+    }
+
+    service {
+        'cgminer':
+            ensure  => running,
+            enable  => true,
+            require => Exec['reload-daemon-config'];
+
+        'catalyst-hook':
+            ensure => running,
+            enable => true;
+    }
 
 }
