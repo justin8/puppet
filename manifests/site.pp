60b833f3f6c8cc1871599d497565002dc6d09e2a
Justin Dray <justin@dray.be>
2014-03-26 08:28:09 +1000

    Removed doge

--- manifests/site.pp
+++ manifests/site.pp
@@ -58,10 +58,6 @@ node 'sugi.dray.be' inherits default {
   include jenkins::slave
 }
 
-node 'doge1.dray.be' inherits default {
-  include doge
-}
-
 node 'wkmil0393.wotifgroup.com' inherits default {
   include collectd::physical
   include collectd::server
