b20e42d153e98f2d288e2a6a096ea705ced453f5
Justin Dray <justin@dray.be>
2014-05-09 00:00:54 +1000

    Added headphones.dray.be proxy

--- manifests/site.pp
+++ manifests/site.pp
@@ -17,6 +17,7 @@ node 'abachi.dray.be' inherits default {
     Httpd::Vhost['abachi.dray.be'],
     Httpd::Vhost['couchpotato.dray.be'],
     Httpd::Vhost['deluge.dray.be'],
+    Httpd::Vhost['headphones.dray.be'],
     Httpd::Vhost['jenkins.dray.be'],
     Httpd::Vhost['repo.dray.be'],
     Httpd::Vhost['sab.dray.be'],
