9991d1dac5610bcf2f718face76459b1abdf7ec2
Justin Dray <justin@dray.be>
2014-06-08 14:51:22 +1000

    Fixed incorrect dconf for disabling screen timeout

--- modules/xbmc/files/standalone/dotfiles/.config/dconf-settings.sh
+++ modules/xbmc/files/standalone/dotfiles/.config/dconf-settings.sh
@@ -4,7 +4,7 @@ dconf write /org/gnome/desktop/wm/preferences/theme "'Adwaita-X-dark'"
 dconf write /org/gnome/desktop/interface/gtk-theme "'Adwaita'"
 dconf write /org/gnome/desktop/interface/icon-theme "'Faenza'"
 dconf write /org/gnome/settings-daemon/plugins/power/sleep-inactive-ac-type "'nothing'"
-dconf write /org/gnome/desktop/session/idle-delay 0
+dconf write /org/gnome/desktop/session/idle-delay 'uint32 0'
 dconf write /org/gnome/desktop/screensaver/lock-enabled false
 dconf write /org/gnome/desktop/background/picture-uri "'file:///$HOME/.background.jpg'"
 dconf write /org/gnome/desktop/background/picture-options "'zoom'"
