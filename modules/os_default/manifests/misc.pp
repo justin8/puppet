9822218aa91ab4e37a1246a0664c202eae22c623
Justin Dray <justin@dray.be>
2014-08-27 17:11:49 +1000

    Moved io optimizations to optimizations manifest

--- modules/os_default/manifests/misc.pp
+++ modules/os_default/manifests/misc.pp
@@ -50,13 +50,4 @@ class os_default::misc {
     subscribe   => File['/etc/locale.gen'],
     refreshonly => true,
   }
-
-  # Fix for slow drives causing hangs
-  # See: http://lwn.net/Articles/572911/
-  sysctl {
-    'vm.dirty_background_bytes': value => '16777216';
-    'vm.dirty_bytes': value => '50331648';
-    # Prioritize inode/dentry cache over block cache
-    'vm.vfs_cache_pressure': value => '50';
-  }
 }
--- modules/os_default/manifests/optimizations.pp
+++ modules/os_default/manifests/optimizations.pp
@@ -23,4 +23,14 @@ class os_default::optimizations {
       ensure  => running,
       enable  => true,
   }
+
+  # Fix for slow drives causing hangs
+  # See: http://lwn.net/Articles/572911/
+  sysctl {
+    'vm.dirty_background_bytes': value => '16777216';
+    'vm.dirty_bytes': value => '50331648';
+    # Prioritize inode/dentry cache over block cache
+    'vm.vfs_cache_pressure': value => '50';
+  }
+
 }
