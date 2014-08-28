e3e543f0f07198448e4f60ac40e07ef79946cb1d
Justin Dray <justin@dray.be>
2014-08-29 09:14:13 +1000

    Small fix for location awareness

--- modules/custom/lib/facter/local.rb
+++ modules/custom/lib/facter/local.rb
@@ -1,7 +1,7 @@
 require 'puppet'
 
 local = 'false'
-if Facter::Util::Resolution.exec('test -f /.remote || nslookup abachi.dray.be|grep 192.168.1.15 && ip route|grep default|grep 192.168.1.1')
+if Facter::Util::Resolution.exec('test -f /.remote || (nslookup abachi.dray.be|grep 192.168.1.15 && ip route|grep default|grep 192.168.1.1)')
   local = 'true'
 end
 Facter.add('local') do
