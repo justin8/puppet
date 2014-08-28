bd6b21ef3d533a1b6f77357ddf28f155ede95f77
Justin Dray <justin@dray.be>
2014-08-29 09:56:41 +1000

    Fixed local using ruby instead of crazy one-liner bash script

--- modules/custom/lib/facter/local.rb
+++ modules/custom/lib/facter/local.rb
@@ -1,9 +1,16 @@
 require 'puppet'
 
 local = 'false'
-if Facter::Util::Resolution.exec('test -f /.remote || (nslookup abachi.dray.be|grep 192.168.1.15 && ip route|grep default|grep 192.168.1.1)')
-  local = 'true'
+if Facter::Util::Resolution.exec('nslookup abachi.dray.be|grep 192.168.1.15')
+  if Facter::Util::Resolution.exec('ip route|grep default|grep 192.168.1.1')
+    local = 'true'
+  end
 end
+
+if Facter::Util::Resolution.exec('test -f /.remote')
+  local = 'false'
+end
+
 Facter.add('local') do
   setcode do
     local
