96b2ffa64de310c0dd6cb25f57a46feb614ffe6c
Justin Dray <justin@dray.be>
2014-09-23 11:06:47 +1000

    Moved preload/prelink config to desktop manifest

--- modules/os_default/manifests/desktop.pp
+++ modules/os_default/manifests/desktop.pp
@@ -1,6 +1,6 @@
 class desktop {
 
-  $packages = [ 'profile-sync-daemon' ]
+  $packages = [ 'preload', 'prelink', 'profile-sync-daemon' ]
   package { $packages: ensure => installed }
 
   #psd.conf config
@@ -11,6 +11,23 @@ class desktop {
   service { 'profile-sync-daemon':
     ensure => running,
     enable => true,
+  }
+
+  file { 'prelink.cron':
+    ensure  => file,
+    path    => '/etc/cron.daily/prelink.cron',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0775',
+    require => Package['prelink', 'cronie'],
+    source  => 'puppet:///modules/os_default/prelink.cron',
+  }
+
+  service {
+    'preload':
+      ensure  => running,
+      enable  => true,
+      require => Package['preload'];
   }
 
 }
--- modules/os_default/manifests/optimizations.pp
+++ modules/os_default/manifests/optimizations.pp
@@ -1,24 +1,5 @@
 class os_default::optimizations {
-  $packages = [ 'prelink', 'preload' ]
-  package { $packages: ensure => installed }
-
-
-  file { 'prelink.cron':
-    ensure  => file,
-    path    => '/etc/cron.daily/prelink.cron',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0775',
-    require => Package['prelink', 'cronie'],
-    source  => 'puppet:///modules/os_default/prelink.cron',
-  }
-
   service {
-    'preload':
-      ensure  => running,
-      enable  => true,
-      require => Package['preload'];
-
     'nscd':
       ensure  => running,
       enable  => true,
