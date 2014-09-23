0b16223e0381b7f60536dfea5d016231a33c44e9
Justin Dray <justin@dray.be>
2014-09-23 11:41:07 +1000

    Changed to recursively manage /etc

--- modules/os_default/manifests/misc.pp
+++ modules/os_default/manifests/misc.pp
@@ -10,39 +10,10 @@ class os_default::misc {
       mode    => '0775',
       source  => 'puppet:///modules/os_default/snap';
 
-    '/etc/puppet/puppet.conf':
-      ensure  => file,
-      mode    => '0664',
-      source  => 'puppet:///modules/os_default/etc/puppet/puppet.conf';
-
-    '/etc/udev/rules.d/50-wol.rules':
-      ensure  => file,
-      mode    => '0664',
-      source  => 'puppet:///modules/os_default/udev_50-wol.rules';
-
-    '/etc/bash.bashrc':
-      ensure  => file,
-      source  => 'puppet:///modules/os_default/etc/bash.bashrc';
-
-    '/etc/DIR_COLORS':
-      ensure  => file,
-      source  => 'puppet:///modules/os_default/etc/DIR_COLORS';
-
-    '/etc/locale.conf':
-      ensure  => file,
-      source  => 'puppet:///modules/os_default/etc/locale.conf';
-
-    '/etc/locale.gen':
-      ensure  => file,
-      source  => 'puppet:///modules/os_default/etc/locale.gen';
-
-    '/etc/ssh/sshd_config':
-      ensure  => file,
-      source  => 'puppet:///modules/os_default/etc/ssh/sshd_config';
-
-    '/etc/updatedb.conf':
-      ensure => file,
-      source => 'puppet:///modules/os_default/etc/updatedb.conf';
+    '/etc':
+      ensure  => directory,
+      recurse => 'remote',
+      source  => 'puppet:///modules/os_default/etc';
   }
 
   exec { 'locale-gen':
