dc0ed455bfb9240ca72f30e63f8d234c8099421f
Justin Dray <justin@dray.be>
2014-10-14 20:58:38 +1000

    Fixed mediacenter::xbmc::gui manifest to not attempt to overwrite variables

--- modules/mediacenter/manifests/xbmc/gui.pp
+++ modules/mediacenter/manifests/xbmc/gui.pp
@@ -2,19 +2,21 @@ define mediacenter::xbmc::gui ( $value, $user, $home_path = undef ) {
 
   if $home_path == undef {
     if $user == 'htpc' {
-      $home_path = "/home/${user}"
+      $real_home_path = "/home/${user}"
     } elsif $user == 'xbmc' {
-      $home_path = '/var/lib/xbmc'
+      $real_home_path = '/var/lib/xbmc'
     } else {
       $home = "home_${user}"
-      $home_path = inline_template("<%= scope.lookupvar('::${home}') %>")
+      $real_home_path = inline_template("<%= scope.lookupvar('::${home}') %>")
     }
+  } else {
+    $real_home_path = $home_path
   }
 
   exec { "xbmc-gui-${title}":
     path    => '/usr/bin',
-    unless  => "grep -qE '<(setting\ .*)?${title}(\ default.*)?\"?>${value}</' '${home_path}/.xbmc/userdata/guisettings.xml'",
-    command => "sed -ri '/<(setting\ .*)?${title}(\ default.*)?\"?>/s/>.*<\//>${value}<\//' '${home_path}/.xbmc/userdata/guisettings.xml'",
+    unless  => "grep -qE '<(setting\ .*)?${title}(\ default.*)?\"?>${value}</' '${real_home_path}/.xbmc/userdata/guisettings.xml'",
+    command => "sed -ri '/<(setting\ .*)?${title}(\ default.*)?\"?>/s/>.*<\//>${value}<\//' '${real_home_path}/.xbmc/userdata/guisettings.xml'",
   }
 
 }
