a247f0eaae100c526acf0271b0920145e94319e7
Justin Dray <justin@dray.be>
2014-10-21 11:24:21 +1000

    First test of remote repo syncing

--- modules/repo/manifests/init.pp
+++ modules/repo/manifests/init.pp
@@ -4,6 +4,39 @@ class repo( $owner = 'http', $group = 'http', $open_network = true) {
   realize ( Httpd::Vhost['repo.dray.be'], )
   $btsync_keys = hiera('btsync_keys')
 
+  if $open_network {
+    $proxy = false
+
+    service { 'repo-proxy':
+      ensure => stopped,
+      enable => false,
+    }
+
+    file { 'repo-proxy.service':
+      ensure  => absent,
+      require => Service['repo-proxy'],
+    }
+  } else {
+    $proxy = true
+
+    package { 'autossh':
+      ensure => present,
+    }
+
+    file { 'repo-proxy.service':
+      path    => '/etc/systemd/system/repo-proxy.service',
+      source  => 'puppet:///modules/repo/repo-proxy.service',
+      require => Package['autossh'],
+    }
+
+    service { 'repo-proxy':
+      ensure => running,
+      enable => true,
+      require => File['repo-proxy.service'],
+    }
+  }
+
+
   package { 'pkgcacheclean':
     ensure => installed,
   }
@@ -43,6 +76,8 @@ class repo( $owner = 'http', $group = 'http', $open_network = true) {
       owner       => $owner,
       group       => $group,
       use_upnp    => $open_network,
+      use_dht     => $open_network,
+      proxy       => $proxy,
       ignore_list => [ 'dray.be.*' ],
       notify      => Service['httpd'];
   }
