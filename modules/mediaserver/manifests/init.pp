768306645150749a94346b5054228774f94dd44c
Justin Dray <justin@dray.be>
2014-10-22 19:23:51 +1000

    cleanup

--- modules/mediaserver/manifests/init.pp
+++ modules/mediaserver/manifests/init.pp
@@ -66,13 +66,4 @@ class mediaserver {
       source => 'puppet:///modules/mediaserver/downloads.conf';
   }
 
-#  include docker
-#
-#  class { [ 'mediaserver::plex',
-#            'mediaserver::couchpotato',
-#            'mediaserver::drone',
-#            'mediaserver::transmission',
-#            'mediaserver::sabnzbd' ]:
-#    config_dir => '/raid/server-files/config',
-#  }
 }
