ff04194297638f24e694a411800a6b316af5d364
Justin Dray <justin@dray.be>
2014-10-26 16:09:19 +1000

    Added cv to default packages

--- modules/os_default/manifests/init.pp
+++ modules/os_default/manifests/init.pp
@@ -7,7 +7,7 @@ class os_default {
   include os_default::ssh
   include os_default::sudo
 
-  $packages = [ 'atop', 'aura-bin', 'avahi', 'dnsutils', 'ethtool', 'git', 'htop', 'iftop', 'mlocate', 'mtr', 'ncdu', 'net-tools', 'nethogs', 'nss-mdns', 'pkgstats', 'rsync', 'pkgfile' ]
+  $packages = [ 'atop', 'aura-bin', 'avahi', 'cv', 'dnsutils', 'ethtool', 'git', 'htop', 'iftop', 'mlocate', 'mtr', 'ncdu', 'net-tools', 'nethogs', 'nss-mdns', 'pkgstats', 'rsync', 'pkgfile' ]
   package { $packages: ensure => installed }
 
   service { ['puppet', 'atop', 'avahi-daemon']:
