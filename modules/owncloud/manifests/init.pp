fd07ead2476ada32c567687a6e6973f5572ccd5c
Justin Dray <justin@dray.be>
2015-02-13 14:43:34 +1000

    Fixed file mode on owncloud backup script

--- modules/owncloud/manifests/init.pp
+++ modules/owncloud/manifests/init.pp
@@ -16,6 +16,7 @@ class owncloud {
 
     '/etc/cron.daily/backup-owncloud-db.cron':
       ensure => present,
+      mode   => '0644',
       source => 'puppet:///modules/owncloud/backup-owncloud-db.cron',
       require => Package['owncloud'];
 
