aa33cc58e6b4d30ecf6ad344f9e84bd8ad1c61c2
Justin Dray <justin@dray.be>
2015-02-20 10:27:34 +1000

    Set all x86 specific things to only happen on x86 hosts

--- modules/os_default/manifests/pacman.pp
+++ modules/os_default/manifests/pacman.pp
@@ -1,8 +1,5 @@
 class os_default::pacman {
 
-  $packages = [ 'cifs-utils', 'smbclient' ]
-  package { $packages: ensure => installed }
-
   cron { 'update-pkgfile':
     command  => 'pkgfile -u &>/dev/null',
     user     => 'root',
@@ -13,36 +10,39 @@ class os_default::pacman {
     month    => '*',
   }
 
-  exec {
-    'configure-repo':
-      path    => '/usr/bin',
-      unless  => 'pacman -Q dray-repo > /dev/null 2>&1',
-      command => 'curl -s "https://repo.dray.be/dray-repo-0.7-1-any.pkg.tar.xz" > /tmp/dray-repo.pkg.tar.xz && pacman --noconfirm -U /tmp/dray-repo.pkg.tar.xz';
-  }
-
   if $architecture == 'x86_64' {
+    exec {
+      'configure-repo':
+        path    => '/usr/bin',
+        unless  => 'pacman -Q dray-repo > /dev/null 2>&1',
+        command => 'curl -s "https://repo.dray.be/dray-repo-0.7-1-any.pkg.tar.xz" > /tmp/dray-repo.pkg.tar.xz && pacman --noconfirm -U /tmp/dray-repo.pkg.tar.xz';
+    }
+
     file { '/etc/pacman.d/mirrorlist':
       ensure => present,
       source => 'puppet:///modules/os_default/etc/pacman.d/mirrorlist';
     }
-  }
 
-  exec {
-    'enable-multilib':
-      path    => '/usr/bin',
-      unless  => 'grep -q "^\[multilib\]" /etc/pacman.conf',
-      command => 'echo -e "\n[multilib]\nInclude = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf';
-  }
+    exec {
+      'enable-multilib':
+        path    => '/usr/bin',
+        unless  => 'grep -q "^\[multilib\]" /etc/pacman.conf',
+        command => 'echo -e "\n[multilib]\nInclude = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf';
+    }
 
-  if "$local" == "true" {
-    if $::hostname != 'abachi' {
-      mount { '/var/cache/pacman/pkg':
-        ensure  => mounted,
-        device  => "//abachi.dray.be/pacman-pkg",
-        fstype  => 'cifs',
-        options => 'credentials=/root/.smbcreds,noauto,x-systemd.automount',
-        atboot  => true,
-        require => Package['cifs-utils'];
+    $packages = [ 'cifs-utils', 'smbclient' ]
+    package { $packages: ensure => installed }
+
+    if "$local" == "true" {
+      if $::hostname != 'abachi' {
+        mount { '/var/cache/pacman/pkg':
+          ensure  => mounted,
+          device  => "//abachi.dray.be/pacman-pkg",
+          fstype  => 'cifs',
+          options => 'credentials=/root/.smbcreds,noauto,x-systemd.automount',
+          atboot  => true,
+          require => Package['cifs-utils'];
+        }
       }
     }
   }
