eeb6bfca3f36b7bf989e66cf4e1a5985e29864b2
Justin Dray <justin@dray.be>
2015-04-20 17:31:46 +1000

    Updated psd to use overlayfs

--- modules/os_default/manifests/desktop.pp
+++ modules/os_default/manifests/desktop.pp
@@ -13,6 +13,9 @@ class os_default::desktop {
 
     '/etc/systemd/system/psd-resync.timer.d/frequency.conf':
       content => '[Timer]\nOnUnitActiveSec=10min\n';
+
+    '/etc/modules-load.d/overlay.conf':
+      content => 'overlay\n';
   }
 
   service { 'psd':
