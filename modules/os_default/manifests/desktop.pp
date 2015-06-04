34fad715517945ab531a572af8de49cb73f0fb4b
Justin Dray <justin@dray.be>
2015-06-05 08:57:01 +1000

    cleaned up old configuration

--- modules/os_default/manifests/desktop.pp
+++ modules/os_default/manifests/desktop.pp
@@ -3,34 +3,6 @@ class os_default::desktop {
   $packages = [ 'preload', 'prelink' ]
   package { $packages: ensure => installed }
 
-
-# CAN BE REMOVED LATER
-
-  package { 'profile-sync-daemon': ensure => absent }
-
-  #psd.conf config
-  file {
-    '/etc/psd.conf':
-      ensure => absent;
-
-    '/etc/systemd/system/psd-resync.timer.d':
-      ensure => absent,
-      force  => true;
-
-    '/etc/systemd/system/psd-resync.timer.d/frequency.conf':
-      ensure => absent;
-
-    '/etc/modules-load.d/overlay.conf':
-      ensure => absent;
-  }
-
-  service { 'psd':
-    ensure => stopped,
-    enable => false,
-  }
-
-####### DO NOT REMOVE PAST HERE
-
   file { '/etc/cron.daily/prelink.cron':
     ensure => absent,
   }
