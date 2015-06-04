26e2aeef5951d7aba588bc6b71d583b3341a232e
Justin Dray <justin@dray.be>
2015-06-05 09:07:56 +1000

    Added support for multiple OSes in cron

--- modules/os_default/manifests/cron.pp
+++ modules/os_default/manifests/cron.pp
@@ -1,9 +1,13 @@
 class os_default::cron {
-  package { 'cronie': ensure => installed }
+  case $::operatingsystem {
+    'Archlinux': { $cron_package = 'cronie'}
+    default: { $cron_package = 'cron' }
+  }
+  package { $cron_package: ensure => installed }
 
-  service { 'cronie':
+  service { $cron_package:
     ensure    => running,
     enable    => true,
-    require   => Package['cronie'],
+    require   => Package[$cron_package],
   }
 }
