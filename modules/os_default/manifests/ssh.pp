b776ae79bd1acff00083e31a52d88815dbf86b9a
Justin Dray <justin@dray.be>
2015-06-05 09:00:29 +1000

    cleaned up old configuration

--- modules/os_default/manifests/ssh.pp
+++ modules/os_default/manifests/ssh.pp
@@ -6,11 +6,4 @@ class os_default::ssh {
     enable    => true,
   }
 
-#  exec { 'sshd-moduli':
-#    unless => "[[ $(found=0; for i in $(cut -d' ' -f5 /etc/ssh/moduli | grep '^[0-9]'); do if [[ $i -lt 200 ]]; then found=1; fi; done; echo $found) == 0 ]]",
-#    command => "ssh-keygen -G /tmp/moduli -b 4096 && ssh-keygen -T /etc/ssh/moduli -f /tmp/moduli && rm /tmp/moduli",
-#    require => Service['haveged'],
-#    logoutput => true,
-#  }
-
 }
