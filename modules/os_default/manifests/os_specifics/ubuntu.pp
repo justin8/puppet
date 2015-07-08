96c16920f5dae52ca9cb0c7daec5e687dfec47be
Justin Dray <justin@dray.be>
2015-07-08 15:29:18 +1000

    Added a resolvconf update

--- modules/os_default/manifests/os_specifics/ubuntu.pp
+++ modules/os_default/manifests/os_specifics/ubuntu.pp
@@ -6,7 +6,7 @@ class os_default::os_specifics::ubuntu {
   package { 'nscd': before => Service['nscd'] }
 
   exec { 'append local domain':
-    command => 'printf "\n\nsearch local dray.be\n" >> /etc/resolvconf/resolv.conf.d/head',
+    command => 'printf "\n\nsearch local dray.be\n" >> /etc/resolvconf/resolv.conf.d/head; resolvconf -u',
     unless  => 'grep -q "search" /etc/resolvconf/resolv.conf.d/head',
   }
 
