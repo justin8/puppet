1fc25ed8984b2582f70a4ef6d61a1503593c463a
Justin Dray <justin@dray.be>
2015-08-31 20:30:12 +1000

    Updated repo module to allow read-only replicas and to work on Ubuntu

--- modules/repo/manifests/init.pp
+++ modules/repo/manifests/init.pp
@@ -1,53 +1,20 @@
-class repo( $owner = 'http', $group = 'http') {
+class repo( $readonly = false) {
   include incron
   $btsync_keys = hiera('btsync_keys')
+  if $operatingsystem == 'Ubuntu' {
+    $owner = 'www-data'
+    $group = 'www-data'
+  } elsif $operatingsystem == 'Archlinux' {
+    $owner = 'http'
+    $group = 'http'
+  }
+
+  validate_bool($readonly)
 
   vhost { 'repo':
     url       => 'repo.dray.be',
     www_root  => '/srv/repo',
     autoindex => 'on',
-  }
-
-  service { 'repo-proxy':
-    ensure => stopped,
-    enable => false,
-  }
-
-  file { 'repo-proxy.service':
-    path    => '/etc/systemd/system/repo-proxy.service',
-    ensure  => absent,
-    require => Service['repo-proxy'],
-  }
-
-  ensure_packages(['pkgcacheclean'])
-
-  incron { 'update-repo':
-    user    => 'root',
-    command => '/usr/local/sbin/update-repo $@/$#',
-    path    => '/srv/repo',
-    mask    => ['IN_CLOSE_WRITE', 'IN_MOVED_TO'],
-  }
-
-  cron { 'pkgcacheclean':
-    command  => 'pkgcacheclean -k 5',
-    minute   => '0',
-    hour     => '4',
-    month    => '*',
-    weekday  => '*',
-    monthday => '*',
-    require  => Package['pkgcacheclean'],
-  }
-
-  logrotate::rule { 'update-repo':
-    path => '/var/log/update-repo.log',
-    rotate => 7,
-    rotate_every => 'day',
-  }
-
-  file {'/usr/local/sbin/update-repo':
-    ensure => file,
-    mode   => '0755',
-    source => 'puppet:///modules/repo/update-repo';
   }
 
   btsync::folder {
@@ -59,5 +26,38 @@ class repo( $owner = 'http', $group = 'http') {
       use_dht     => $open_network,
       ignore_list => [ 'dray.be.*' ],
       notify      => Service['nginx'];
+  }
+
+  if $readonly == False {
+    ensure_packages(['pkgcacheclean'])
+
+    incron { 'update-repo':
+      user    => 'root',
+      command => '/usr/local/sbin/update-repo $@/$#',
+      path    => '/srv/repo',
+      mask    => ['IN_CLOSE_WRITE', 'IN_MOVED_TO'],
+    }
+
+    cron { 'pkgcacheclean':
+      command  => 'pkgcacheclean -k 5',
+      minute   => '0',
+      hour     => '4',
+      month    => '*',
+      weekday  => '*',
+      monthday => '*',
+      require  => Package['pkgcacheclean'],
+    }
+
+    logrotate::rule { 'update-repo':
+      path => '/var/log/update-repo.log',
+      rotate => 7,
+      rotate_every => 'day',
+    }
+
+    file {'/usr/local/sbin/update-repo':
+      ensure => file,
+      mode   => '0755',
+      source => 'puppet:///modules/repo/update-repo';
+    }
   }
 }
