67cf70ae8a3bd7ac3cf2fca408ca514a33844832
Justin Dray <justin@dray.be>
2015-08-31 19:31:21 +1000

    Fixed duplicate notify

--- modules/openvpn/manifests/init.pp
+++ modules/openvpn/manifests/init.pp
@@ -41,8 +41,7 @@ class openvpn {
 
     file { "/etc/systemd/system/openvpn@.service":
       source => 'puppet:///modules/openvpn/openvpn@.service',
-      notify => Exec['systemd-daemon-reload'],
-      notify => Exec['maintain-vpn']
+      notify => Exec['systemd-daemon-reload', 'maintain-vpn'],
     }
 
     file { "/usr/local/sbin/maintain-vpn":
