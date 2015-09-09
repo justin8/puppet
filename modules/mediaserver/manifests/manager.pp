b2c378d456ee5f8c4456c6c406b10361e70b2eec
Justin Dray <justin@dray.be>
2015-09-09 16:27:02 +1000

    Fixed typo

--- modules/mediaserver/manifests/manager.pp
+++ modules/mediaserver/manifests/manager.pp
@@ -30,7 +30,7 @@ class mediaserver::manager {
       require => [
         File['/etc/systemd/system/couchpotato.service.d/downloads.conf'],
         Package['couchpotato'],
-      };
+      ];
 
     'plexmediaserver':
       ensure  => running,
@@ -38,15 +38,15 @@ class mediaserver::manager {
       require => [
         File['/etc/systemd/system/plexmediaserver.service.d/downloads.conf'],
         Package['plex-media-server'],
-      };
+      ];
 
     'sonarr':
       ensure  => running,
       enable  => true,
       require => [
         File['/etc/systemd/system/sonarr.service.d/downloads.conf'],
-        Package['sonarr'],
-      };
+        Package['sonarr-develop'],
+      ];
   }
 
   file {
