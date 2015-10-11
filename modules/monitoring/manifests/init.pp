c522fdf6afb2a35509df2d4092c5b09b4f17722b
Justin Dray <justin@dray.be>
2015-10-11 23:28:23 +0200

    Fix telegraf on ubuntu

--- modules/monitoring/manifests/init.pp
+++ modules/monitoring/manifests/init.pp
@@ -19,8 +19,8 @@ class monitoring {
       ensure_packages(['telegraf'])
     }
     'Ubuntu': {
-      $package_source_name = "https://s3.amazonaws.com/get.influxdb.org/telegraf/telegraf_0.1.9_amd64.deb"
-      $package_source = "http://get.influxdb.org/telegraf/${package_source_name}"
+      $package_source_name = "telegraf_0.1.9_amd64.deb"
+      $package_source = "https://s3.amazonaws.com/get.influxdb.org/telegraf/${package_source_name}"
       wget::fetch { 'telegraf':
         source      => $package_source,
         destination => "/tmp/${package_source_name}"
@@ -37,17 +37,35 @@ class monitoring {
     }
   }
 
-  file { '/etc/telegraf/telegraf.conf':
-    ensure  => present,
-    mode    => '0644',
-    content => template('monitoring/telegraf.conf.erb'),
-    require => Package['telegraf'],
+  file {
+    '/etc/telegraf':
+      ensure => directory;
+
+    '/etc/telegraf/telegraf.conf':
+      ensure  => present,
+      mode    => '0644',
+      content => template('monitoring/telegraf.conf.erb'),
+      require => Package['telegraf'];
+  }
+
+  file {
+    '/etc/systemd/system/telegraf.service.d':
+      ensure => directory;
+
+    '/etc/systemd/system/telegraf.service.d/config.conf':
+      ensure => present,
+      source => 'puppet:///modules/monitoring/systemd-service.conf',
+      notify => Exec['systemd-daemon-reload'];
   }
 
   service { 'telegraf':
     ensure  => running,
     enable  => true,
-    require => Package['telegraf'];
+    require => [
+      Package['telegraf'],
+      File['/etc/systemd/system/telegraf.service.d/config.conf'],
+      Exec['systemd-daemon-reload'],
+    ];
   }
 
   #diamond::collector {
