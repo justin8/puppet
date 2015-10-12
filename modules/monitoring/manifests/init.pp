ad1b1e9e6381b78c41af8fd33f4f6c0404d43b06
Justin Dray <justin@dray.be>
2015-10-12 22:32:02 +0200

    Fix telegraf service

--- modules/monitoring/manifests/init.pp
+++ modules/monitoring/manifests/init.pp
@@ -1,24 +1,11 @@
 class monitoring {
-  #if $operatingsystem != 'Archlinux' {
-  #  $install_from_pip = true
-  #} else {
-  #  $install_from_pip = false
-  #}
-
-  #class { 'diamond':
-  #  graphite_host    => 'abachi.local',
-  #  interval         => 10,
-  #  purge_handlers   => true,
-  #  purge_collectors => true,
-  #  install_from_pip  => $install_from_pip
-  #}
-
-  # Change to telegraf module in the future
   case $operatingsystem {
     'Archlinux': {
+      $config_location = '/etc/telegraf/telegraf.conf'
       ensure_packages(['telegraf'])
     }
     'Ubuntu': {
+      $config_location = '/etc/opt/telegraf/telegraf.conf'
       $package_source_name = "telegraf_0.1.9_amd64.deb"
       $package_source = "https://s3.amazonaws.com/get.influxdb.org/telegraf/${package_source_name}"
       wget::fetch { 'telegraf':
@@ -37,25 +24,11 @@ class monitoring {
     }
   }
 
-  file {
-    '/etc/telegraf':
-      ensure => directory;
-
-    '/etc/telegraf/telegraf.conf':
-      ensure  => present,
-      mode    => '0644',
-      content => template('monitoring/telegraf.conf.erb'),
-      require => Package['telegraf'];
-  }
-
-  file {
-    '/etc/systemd/system/telegraf.service.d':
-      ensure => directory;
-
-    '/etc/systemd/system/telegraf.service.d/config.conf':
-      ensure => present,
-      source => 'puppet:///modules/monitoring/systemd-service.conf',
-      notify => Exec['systemd-daemon-reload'];
+  file { $config_location:
+    ensure  => present,
+    mode    => '0644',
+    content => template('monitoring/telegraf.conf.erb'),
+    require => Package['telegraf'];
   }
 
   service { 'telegraf':
@@ -63,30 +36,8 @@ class monitoring {
     enable  => true,
     require => [
       Package['telegraf'],
-      File['/etc/systemd/system/telegraf.service.d/config.conf'],
+      File[$config_location],
       Exec['systemd-daemon-reload'],
     ];
   }
-
-  #diamond::collector {
-  #  ['CPUCollector',
-  #   'DiskSpaceCollector',
-  #   'DiskUsageCollector',
-  #   'MemoryCollector',
-  #   'VMStatCollector',
-  #   'LoadAverageCollector',
-  #   'PuppetAgentCollector']: ;
-
-  # 'NetworkCollector':
-  #   options => {'interfaces' => 'eth, bond, ens, enx, enp, eno'};
-  #}
-
-
-
-  # Service-based auto-collection
-  #if $is_virtual == 'false' {
-  #  package { 'python2-pysensors': ensure => installed }
-  #  diamond::collector { 'LMSensorsCollector': }
-  #}
-
 }
