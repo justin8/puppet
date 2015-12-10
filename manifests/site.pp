f95c50d77812289f2b9d66d83c4939094f908340
Justin Dray <justin@dray.be>
2015-12-10 22:24:01 +0100

    Add hickory

--- manifests/site.pp
+++ manifests/site.pp
@@ -79,7 +79,7 @@ node 'ironwood.dray.be' {
   include os_default::desktop
 }
 
-node /^cypress.*/ {
+node /^(cypress|hickory).*/ {
   include os_default
   include os_default::mail
   include openvpn
